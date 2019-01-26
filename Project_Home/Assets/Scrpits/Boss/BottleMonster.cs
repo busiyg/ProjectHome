10feda1ac58749bba6fbc6530c23aad8c59dc769
zhongxiangyu <zh9244@163.com>
2019-01-26 19:22:45 +0800

    123123

---
+++ Project_Home/Assets/Scrpits/Boss/BottleMonster.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using DG.Tweening;
+using UnityEngine;
+
+public class BottleMonster : MonoBehaviour
+{
+    public float Rate;
+
+    private float _speed;
+    public float ASpeed;
+    public SpriteRenderer BottleImg;
+    public float HurtTime;
+
+
+    private Transform _target;
+    public Animator Anim;
+
+    private float angle;
+
+    private string stateName;
+
+    private int _bloodNum = 3;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _target = GameObject.FindWithTag("Player").transform;
+
+        CutAim();
+    }
+    
+    void Update ()
+	{
+	    if (Input.GetKeyDown(KeyCode.E))
+	    {
+	        CutState("Shoot");
+        }
+
+	    if (Input.GetKeyDown(KeyCode.R))
+	    {
+	        if (stateName == "Dizzy" && !_isHurt)
+	        {
+	            StartCoroutine(HurtCoroutine());
+            }
+        }
+
+	    if (_bloodNum == 0)
+	    {
+            gameObject.SetActive(false);
+	    }
+
+	    switch (stateName)
+	    {
+	        case "Aim":
+	            Aim();
+                break;
+	        case "Shoot":
+	            Shoot();
+                break;
+            case "Dizzy":
+                Dizzy();
+                break;
+	    }
+    }
+
+    void Aim()
+    {
+        angle = Mathf.Rad2Deg * Mathf.Atan((transform.position.y - _target.position.y) / (transform.position.x - _target.position.x));
+
+        if (transform.position.x - _target.position.x < 0)
+            angle = angle - 90;
+
+        else
+            angle = angle + 90;
+
+        transform.localEulerAngles = new Vector3(0, 0, angle);
+
+        if (Anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1.0f && Anim.GetCurrentAnimatorStateInfo(0).IsName("BottlePrepare"))
+        {
+            CutShot();
+        }
+    }
+
+    void Shoot()
+    {
+        _speed += ASpeed;
+        transform.position += transform.up * _speed * Time.deltaTime;
+    }
+
+    private float _dizzyTime = 0;
+    public float DizzyIntervalTime;
+    private bool _isHurt;
+    void Dizzy()
+    {
+        //播放眩晕特效
+
+        if (_dizzyTime < DizzyIntervalTime)
+        {
+            _dizzyTime += Time.deltaTime;
+        }
+        else
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        Anim.SetTrigger("Rotate");
+        if (Anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1.0f && Anim.GetCurrentAnimatorStateInfo(0).IsName("BottleRotate"))
+        {
+            _dizzyTime = 0;
+            Anim.SetBool("Fire", false);
+            Anim.ResetTrigger("Idle");
+            Anim.ResetTrigger("Rotate");
+            _isHurt=false;
+            CutAim();
+        }
+    }
+
+    IEnumerator HurtCoroutine()
+    {
+        _isHurt = true;
+        _bloodNum--;
+        BottleImg.DOColor(Color.red, HurtTime);
+        yield return new WaitForSeconds(HurtTime);
+        BottleImg.DOColor(Color.white, HurtTime);
+
+        _dizzyTime = DizzyIntervalTime + 1;
+    }
+
+    void CutAim()
+    {
+        _speed = 0;
+        stateName = "Aim";
+    }
+
+    void CutShot()
+    {
+        Anim.SetBool("Fire", true);
+        stateName = "Shoot";
+    }
+
+    void CutDizzy()
+    {
+        _speed = 0;
+        Anim.SetTrigger("Idle");
+        stateName = "Dizzy";
+    }
+    
+    void CutState(string name)
+    {
+        switch (name)
+        {
+            case "Aim":
+                
+                break;
+            case "Shoot":
+                
+                break;
+            case "Dizzy":
+                _speed = 0;
+                Anim.SetTrigger("Idle");
+                stateName = name;
+                break;
+            case "Normal":
+                Anim.SetBool("Fire", false);
+                _speed = 0;
+                stateName = "Aim";
+                break;
+            case "HurtCoroutine":
+                StartCoroutine(HurtCoroutine());
+                break;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.name == "BackGround")
+        {
+            if (_speed >= 22)
+            {
+                if (stateName == "Dizzy" || stateName == "Normal")
+                    return;
+
+                Debug.Log("眩晕");
+                CutDizzy();
+            }
+            else
+            {
+                if (stateName == "Dizzy" || stateName == "Normal")
+                    return;
+
+                Debug.Log("不眩晕");
+                Anim.SetBool("Fire", false);
+                _speed = 0;
+                CutAim();
+            }
+        }
+
+        if (collider.name == "shitou")
+        {
+            if (stateName == "Dizzy" && !_isHurt)
+                CutState("HurtCoroutine");
+        }
+    }
+}
---
+++ Project_Home/Assets/Scrpits/Boss/MouthEarMonster.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class MouthEarMonster : MonoBehaviour
+{
+    public GameObject ShotCtrl;
+    public float Speed;
+    public float RotateSpeed;
+    public Transform MouthMonstImg;
+    public float AimTime;
+    public float ShootTime;
+
+    public bool IsShot;
+
+    private string _stateName;
+    private Transform _target;
+
+    private float angle;
+
+    // Use this for initialization
+    void Start ()
+	{
+	    _target = GameObject.FindWithTag("Player").transform;
+
+	    CutAim();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    switch (_stateName)
+	    {
+            case "Aim":
+                Aim();
+                break;
+            case "Shoot":
+                Shoot();
+                //if (!IsShot)
+                //{
+                    
+                //}
+                //else
+                //{
+
+                //}
+                break;
+            case "Dizzy":
+                break;
+        }
+
+
+	    if (Vector3.Dot(Vector3.up, new Vector3(transform.position.y - _target.position.y, transform.position.x - _target.position.x, 0)) > 0)
+	    {
+	        Debug.Log("左边");
+	        Debug.DrawRay(transform.position, transform.right * -100, Color.red);
+	        Debug.DrawRay(transform.position, Vector3.up * 100, Color.red);
+        }
+	    if (Vector3.Dot(Vector3.up, new Vector3(transform.position.y - _target.position.y, transform.position.x - _target.position.x, 0)) < 0)
+	    {
+	        Debug.Log("右边");
+	        Debug.DrawRay(transform.position, transform.right * 100, Color.red);
+	        Debug.DrawRay(transform.position, Vector3.up * 100, Color.red);
+        }
+        
+	}
+
+    void CutAim()
+    {
+        _stateName = "Aim";
+    }
+
+    void CutShoot()
+    {
+        MouthMonstImg.rotation=Quaternion.identity;
+        MouthMonstImg.GetComponent<SpriteRenderer>().flipX = false;
+        ShotCtrl.SetActive(true);
+        //
+        _stateName = "Shoot";
+        
+    }
+
+    private float _aimTime;
+    void Aim()
+    {
+        if (Vector3.Dot(transform.up, new Vector3(transform.position.y - _target.position.y, transform.position.x - _target.position.x,  0)) > 0)
+        {
+            Debug.Log("左边");
+            MouthMonstImg.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if (Vector3.Dot(transform.up, new Vector3(transform.position.y - _target.position.y, transform.position.x - _target.position.x,  0)) < 0)
+        {
+            Debug.Log("右边");
+            MouthMonstImg.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    
+        Vector2 dir = _target.position - transform.position;
+        transform.Translate(dir * Speed * Time.deltaTime);
+
+        if (_aimTime < AimTime)
+        {
+            _aimTime += Time.deltaTime;
+        }
+        else
+        {
+            CutShoot();
+            _aimTime = 0;
+        }
+    }
+
+    private float _shootTime;
+    void Shoot()
+    {
+        float bulletAngle = 0;
+        if (Vector3.Dot(Vector3.up, new Vector3(transform.position.y - _target.position.y, transform.position.x - _target.position.x, 0)) > 0)
+        {
+            Debug.Log("左边");
+            MouthMonstImg.GetComponent<SpriteRenderer>().flipX = false;
+            angle = Mathf.Rad2Deg * Mathf.Atan((transform.position.y - _target.position.y) / (transform.position.x - _target.position.x));
+
+            if (transform.position.x - _target.position.x < 0)
+            {
+                angle = angle - 180;
+            }
+
+            bulletAngle = Vector3.Angle(-transform.right, Vector3.up);
+
+
+        }
+        if (Vector3.Dot(Vector3.up, new Vector3(transform.position.y - _target.position.y, transform.position.x - _target.position.x, 0)) < 0)
+        {
+            Debug.Log("右边");
+            MouthMonstImg.GetComponent<SpriteRenderer>().flipX = true;
+
+            angle = Mathf.Rad2Deg * Mathf.Atan((transform.position.y - _target.position.y) / (transform.position.x - _target.position.x));
+
+            if (transform.position.x - _target.position.x < 0)
+            {
+                //angle = angle - 180;
+            }
+
+            bulletAngle = Vector3.Angle(transform.right, Vector3.up);
+
+            bulletAngle = 360 - bulletAngle;
+        }
+
+        ShotCtrl.GetComponent<UbhShotCtrl>()._ShotList[0]._ShotObj.GetComponent<UbhPaintShot>()._PaintCenterAngle = bulletAngle;
+
+
+        transform.localEulerAngles = new Vector3(0, 0, angle);
+
+        if (_shootTime < ShootTime)
+        {
+            _shootTime += Time.deltaTime;
+        }
+        else
+        {
+            //ShotCtrl.SetActive(true);
+            //CutShoot();
+            //_aimTime = 0;
+        }
+    }
+}
