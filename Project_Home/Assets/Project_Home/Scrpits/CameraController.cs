e7f0dbb1c7474eb491ad8c840d7b50f86c6914e3
Mr147 <busiyg@gmail.com>
2019-01-26 11:30:59 +0800

    Init Project

---
+++ Project_Home/Assets/Project_Home/Scrpits/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform Target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CameraFollow();
+
+    }
+
+    public void CameraFollow() {
+        var TarPos = new Vector3(Target.position.x, Target.position.y+2, -10);
+        transform.position = Vector3.Lerp(transform.position, TarPos, 0.1f);
+    }
+}
---
+++ Project_Home/Assets/Project_Home/Scrpits/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float SpeedScale;
+    public float JumpSpeed;
+    public bool IsJump;
+    public Rigidbody2D PlayerRigidbody;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HorizontialMove();
+        VerticalMove();
+    }
+
+    public void HorizontialMove() {
+        transform.Translate(Input.GetAxis("Horizontal") * SpeedScale,0,0);
+    }
+
+    public void VerticalMove() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("space");
+            PlayerRigidbody.AddForce(Vector2.up*JumpSpeed);
+        }
+
+    }
+}
